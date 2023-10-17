e0555e8cd1f50986d25d94b8b23bdf7378c4a95a
Igor Lima <igordtbeta2019@gmail.com>
2023-10-17 01:02:28 -0300

    Ajustando tipo do ID na interface Repository de Integer para UUID.

--- src/main/java/com/gcpTest/gcp/database/repository/UsuarioRepository.java
+++ src/main/java/com/gcpTest/gcp/database/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.gcpTest.gcp.database.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gcpTest.gcp.database.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 	
 	
 	
